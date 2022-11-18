1544799bdd886f6ebf30e6957a936853efab0c8c
wgroeneveld <wouter@brainbaking.com>
2022-11-18 10:58:39 +0100

    fix resource path voor Windows gebruikers

--- examples/java/concurrency/src/main/java/be/kuleuven/concurrency/jdbc/ConnectionManager.java
+++ examples/java/concurrency/src/main/java/be/kuleuven/concurrency/jdbc/ConnectionManager.java
@@ -1,5 +1,6 @@
 package be.kuleuven.concurrency.jdbc;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -9,7 +10,8 @@ public class ConnectionManager {
     public static final String ConnectionStringH2 = "jdbc:h2:./mydb.h2db";
 
     public static void initTables(Connection connection) throws Exception {
-        var sql = new String(Files.readAllBytes(Paths.get(ConnectionManager.class.getResource("/dbcreate.sql").getPath())));
+        var path = new File(ConnectionManager.class.getResource("/dbcreate.sql").getFile()).toPath();
+        var sql = new String(Files.readAllBytes(path));
         System.out.println(sql);
 
         var s = connection.createStatement();
